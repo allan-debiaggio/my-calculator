5e0455c7574c4568e4c72977557d9ec11fbf1f19
allan.debiaggio <allan.de-biaggio@laplateforme.io>
2025-01-14 06:28:04 +0100

    Implementing functions to first draft

--- calculator.py
+++ calculator.py
@@ -1,23 +1,44 @@
-def menu() :
+def calculator() :
     print ("Hello ! Welcome to the calculator ! :D")
 
-    number_type=input("Type 1 to use integers, 2 to use float : ")
-    if number_type == "1" :
-        number1 = int(input("Give the first number you want to use : "))
-        number2 = int(input("Give the second number you want to use : "))
-    elif number_type == "2" :
-        number1 = float(input("Give the first number you want to use : "))
-        number2 = float(input("Give the second number you want to use : "))
+def number_type():
+    while True :
+        try :
+            user_choice = input("Type 1 to use integers, 2 to use floats : ")
+            if user_choice == "1" :
+                number1= int(input("Give the first number you want to use : "))
+                number2= int(input("Give the second number you want to use : "))
+            elif user_choice == "2" :
+                number1= float(input("Give the first number you want to use : "))
+                number2 = float(input("Give the second number you want to use : "))
+            else :
+                print("I did not understand your request.")
+                continue
+        except KeyboardInterrupt : 
+            print("Exiting calculator...")
+            break
+        return number1,number2
 
-    operator=input("Give the operator you want to use between \n + (add), -(minus), /(divide), *(multiply): ")
-    result_phrase="The result is "
-    if operator == "+" :
-        print(result_phrase + str(number1 + number2))
-    if operator == "-" :
-        print(result_phrase + str(number1 - number2))
-    elif operator == "*" :
-        print(result_phrase + str(number1 * number2))
-    elif operator == "/" :
-        print(result_phrase + str(number1 / number2))
+def operator(a,b):
+    while True :
+        try : 
+            user_choice = input("Type the operator you want to use : \n+(add), -(minus),*(multiply),/(divide) : ")
+            if user_choice == "+" :
+                return a + b
+            elif user_choice == "-" :
+                return a - b
+            elif user_choice == "*" :
+                return a * b
+            elif user_choice == "/" :
+                return a / b
+            else : 
+                print("I did not understand your request.")
+                continue
+        except KeyboardInterrupt :
+            print("Exiting calculator...")
+            break
+    
 
-menu()
+calculator()
+number1, number2 = number_type()
+print(operator(number1,number2))
